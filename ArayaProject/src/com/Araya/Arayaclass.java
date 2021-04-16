6528c9ad108c89984dcfcb5ba7f626b68ff075ed
asge2009 <82427068+asge2009@users.noreply.github.com>
2021-04-15 22:39:20 -0400

    add

---
+++ ArayaProject/src/com/Araya/Arayaclass.java
@@ -0,0 +1,12 @@
+package com.Araya;
+
+public class Arayaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello everyone");
+		
+	}
+
+}
